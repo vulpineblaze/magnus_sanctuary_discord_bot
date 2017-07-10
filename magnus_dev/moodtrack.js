3bb8345c693d8cf93f63440ba259f1114504965d
Jonathan Giacomelli <jonathan_m_g@live.com>
2017-07-10 10:37:03 -0700

    Update moodtrack.js

--- magnus_dev/moodtrack.js
+++ magnus_dev/moodtrack.js
@@ -1,15 +1,42 @@
 
+const MongoClient = require('mongodb').MongoClient
+
+
+var db
+var configDB = require('./database.js');
+
+
 var check_and_store = function(message){
 	// userlist = load_json();
     var str = message.content.toLowerCase();
 	if(str.charAt(0)==':'){
-	    var res = str.split(":");
+        var res = str.split(":");
         var mood = res[1];
         var number = res[2];
         var desc = res[3];
 //         console.log(str+"|"+mood+"|"+number+"|"+desc);
         message.channel.sendMessage("Your mood is: "+mood+"\nYour number is: "+number+"\nYour desc is: "+desc);
-        message.channel.sendMessage("I have no current saving capacity.");
+//         message.channel.sendMessage("I have no current saving capacity.");
+        
+        var dict = []; // create an empty array
+
+        dict.push({
+            server:   message.guild,
+            user: message.author,
+            mood: mood,
+            number: number,
+            desc: desc
+        });
+        
+        MongoClient.connect(configDB.url, (err, database) => {
+          if (err) return console.log(err)
+          db = database
+          db.collection('moodtrack').save(dict, (err, result) => {
+              if (err) return console.log(err)
+              console.log('saved to database')
+              message.channel.sendMessage("Your entry has been saved to the database.");
+            })
+        })
 	}
     
     
