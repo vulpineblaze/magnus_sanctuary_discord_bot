48c5058a23fa349458e3ee2348b96198837676ea
Jonathan Giacomelli <jonathan_m_g@live.com>
2017-07-10 11:15:39 -0700

    Update linksave.js

--- magnus_dev/linksave.js
+++ magnus_dev/linksave.js
@@ -7,7 +7,27 @@ var configDB = require('./database.js');
 
 
 var check_and_respond = function(message){
-	console.log(message.attachments);
+	if(message.attachments){
+    
+        var dict = []; // create an empty array
+        dict.push({
+            link:   message.attachments.proxyURL.toString()
+        });
+        
+        MongoClient.connect(configDB.url, (err, database) => {
+          if (err) return console.log(err)
+
+            database.collection('moodtrack', function(err, collection) {
+                collection.insert(dict, {safe:true}, function(err, result) {
+                     console.log('saved to database: '+JSON.stringify(dict))
+                    message.channel.sendMessage("Your pictures link has been saved to the database.");
+                })      
+            })
+        })
+
+    
+    
+    }//end if
 	
 }
 
