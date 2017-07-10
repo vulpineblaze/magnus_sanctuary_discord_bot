78c9aa16f63c1459c8a468628481dc9a50b4003f
Jonathan Giacomelli <jonathan_m_g@live.com>
2017-07-10 10:04:42 -0700

    Update moodtrack.js

--- magnus_dev/moodtrack.js
+++ magnus_dev/moodtrack.js
@@ -1,7 +1,16 @@
 
 var check_and_store = function(message){
 	// userlist = load_json();
-
+    var str = message.content.toLowerCase();
+	if(str.charAt(0)==':'){
+	    var res = str.split(":");
+        var mood = res[0];
+        var number = res[1];
+        var desc = res[2];
+	}
+    console.log(str+"|"+res);
+	
+	
 
 }
 
