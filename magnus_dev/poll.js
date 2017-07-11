0039929c4f4102372aed08593192d7cc3bf84657
Jonathan Giacomelli <jonathan_m_g@live.com>
2017-07-11 09:13:41 -0700

    Update poll.js

--- magnus_dev/poll.js
+++ magnus_dev/poll.js
@@ -6,7 +6,7 @@
 
 var fs = require('fs');
 var userlist = "";
-var poll = {};
+var poll = [];
 
 var util = require('util');
 
@@ -59,7 +59,7 @@ var process_poll = function(message,delim="!cookie"){
                 poll.push({poll:quote[1],
                           user:author});
                 retval = "Poll is now:\n"
-                            +poll.poll;
+                            +poll[0].poll;
             }
         }
 	}else if (res[1]=="option"){
@@ -67,7 +67,7 @@ var process_poll = function(message,delim="!cookie"){
 	        poll.push({option:quote[1]});
         }
 	}else if(res[1]=="clear"){
-        poll = {};
+        poll = [];
     }
 		  
 	console.log(author+"|"+res+"|"+quote+"\n"+JSON.stringify(poll));
