425d1f1fe2a4fcd10da81ef316b1bf5baba8a7b1
Jonathan Giacomelli <jonathan_m_g@live.com>
2017-07-24 10:41:45 -0700

    Update cookie.js

--- magnus_dev/cookie.js
+++ magnus_dev/cookie.js
@@ -1,19 +1,9 @@
 
-// var userdict = {};
-// dict.key1 = "value1";
-// dict.key2 = "value2";
-
 var fs = require('fs');
 var userlist = "";
 
 var util = require('util');
 
-// function save_json(obj){
-// 	fs.writeFile('./userlist.json', JSON.stringify(obj, null, 2),function (err, data) {
-// 	  if (err) throw err; // we'll not consider error handling for now
-// 	  console.log("saved json: "+data);
-// 	});
-// }
 function save_json(obj){
 	fs.writeFile('./userlist.json', JSON.stringify(obj, null, 2),function (err, data) {
 	  if (err) throw err; // we'll not consider error handling for now
@@ -44,7 +34,13 @@ var give_cookie = function(message,delim="!cookie"){
 	var user = result.match(/\S+/gi);
 	console.log("user:"+user);
 	if (user){}else{
-		return " \nPlease @mention a user deserving a cookie."
+        // add text will append error msg with users curent cookie count
+		var add_text = "";
+        if(userlist[user]){
+			value = userlist[user]; // doesnt update inc correctly
+            add_text = "\n     You have "+value+ "cookies.";
+		} 
+		return " \nPlease @mention a user deserving a cookie."+add_text
 	}
 
 	var value = 1;
@@ -67,56 +63,12 @@ var give_cookie = function(message,delim="!cookie"){
 
 	save_json(userlist);
 
-	// var linkable_user = message.guild.members.find('user.username', user);
-	// console.log(Discord);
+
 	var linkable_user = user;
-	// var members = message.guild.members;
-	// var mentions = message.mentions.users;
-	// console.log("members:"+members);
-	// console.log("mentions:"+mentions);
-	// members.forEach(function(item) { /* etc etc */
-	// 	console.log(item.id+" vs "+user); 
-	// 	if(item.username == user){
-	// 		linkable_user = item;
-	// 	}else{
-	// 		// nope
-	// 	}
-	// });
-
-	// Object.keys(members).forEach(function(key) {
-	//   var item = members[key];
-	//   console.log(item+" vs "+user);
-	// });
-	// Object.keys(mentions).forEach(function(key) {
-	//   var item = mentions[key];
-	//   console.log(item+" vs "+user);
-	// });
-
-	// console.log("linkable_user:"+linkable_user)
-	// console.log("author:"+message.author)
 	var ret_string = linkable_user+" now has "+value+" cookies!"
 	return ret_string;
-	// var time_stamp = Date.now() / 1000 | 0;
-	// userdict[author]=time_stamp;
+
 }
-
-
-// var time_stamp = function(author){
-// 	var time_stamp = Date.now() / 1000 | 0;
-// 	userdict[author]=time_stamp;
-// }
-
-
-// var show_all_stamps = function(author){
-// 	var ts_string = "";
-//   	for (var key in userdict) {
-// 	  var value = userdict[key];
-// 	  var ts = new Date(value*1000).toISOString().slice(0, -5);
-// 	  ts_string = ts_string + key+" was "+ ts + "\n";
-// 	    // Use `key` and `value`
-// 	}
-// 	return ts_string;
-// }
 
 
 module.exports = {
