0ae07f3c2e943e25a0e0f5e0dd722109fca5b320
Jonathan Giacomelli <jonathan_m_g@live.com>
2017-08-03 10:30:06 -0700

    Update calendar.js

--- magnus_dev/calendar.js
+++ magnus_dev/calendar.js
@@ -5,7 +5,6 @@ var configDB = require('./database.js');
 
 function push_to_db(message, quote, timeshift){
 	
-    var ret_string = "Push to the database failed..."
     var dict = []; // create an empty array
     var user_string = message.author.toString().replace(/[\<\>\@]/g,'');
     var ts = new Date( (Date.now() / 1000 | 0)*1000 ).toISOString().slice(0, -5);
@@ -22,13 +21,12 @@ function push_to_db(message, quote, timeshift){
         database.collection('calendar', function(err, collection) {
             collection.insert(dict, {safe:true}, function(err, result) {
                 console.log('saved to database: '+JSON.stringify(dict))
-//                 message.author.send("Your entry has been saved to the database.\n"
-//                                     +"magnus.bot.nu/m"+user_string);
-                ret_string = "Your entry has been saved to the database!";
+//                 message.author.send("Your entry has been saved to the database.\n");
+//                 return "Your entry has been saved to the database!";
             })      
         })
     })
-    return ret_string;
+    return "Your entry \""+quote+"\" has been processed.";
 }
 
 function pull_from_db(){
@@ -69,7 +67,7 @@ var process_calendar = function(message,delim="!calendar"){
         has_quote=true; // to prevent extra error message
     }else if(has_quote && (param == "daily" 
                            || param == "d")){
-        ret_string = push_to_db(message, quote, 0);
+        push_to_db(message, quote[1], 0);
     }else if(param == "monthly" || param == "m"){
     }else if(param == "weekly" || param == "w"){
     }else if(param == "yearly" || param == "y"){
